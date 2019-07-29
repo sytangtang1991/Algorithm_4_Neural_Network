f99b8f67887da0339289d4f16f42de1ef809c7d0
sytangtang1991 <53004878+sytangtang1991@users.noreply.github.com>
2019-07-28 20:59:47 -0500

    Update NN.py

--- NN.py
+++ NN.py
@@ -253,29 +253,7 @@ print(test_parameters_update['b2'])
 N_SAMPLES = 1000
 X, y = make_moons(n_samples = N_SAMPLES, noise=0.2, random_state=100)
 
-################################################################
-# Visualization: the function making up the graph of a dataset
-################################################################
-def make_plot(X, y, plot_name, file_name=None, XX=None, YY=None, preds=None, dark=False):
-    if (dark):
-        plt.style.use('dark_background')
-    else:
-        sns.set_style("whitegrid")
-    plt.figure(figsize=(16,12))
-    axes = plt.gca()
-    axes.set(xlabel="$X_1$", ylabel="$X_2$")
-    plt.title(plot_name, fontsize=30)
-    plt.subplots_adjust(left=0.20)
-    plt.subplots_adjust(right=0.80)
-    if(XX is not None and YY is not None and preds is not None):
-        plt.contourf(XX, YY, preds.reshape(XX.shape), 25, alpha = 1, cmap=cm.Spectral)
-        plt.contour(XX, YY, preds.reshape(XX.shape), levels=[.5], cmap="Greys", vmin=0, vmax=.6)
-    plt.scatter(X[:, 0], X[:, 1], c=y.ravel(), s=40, cmap=plt.cm.Spectral, edgecolors='black')
-    if(file_name):
-        plt.savefig(file_name)
-        plt.close()
 
-make_plot(X, y, "Dataset")
 
 
 #######################################
